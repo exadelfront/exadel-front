23e3d1c4279edecb1b8dab867afbd8d776bf2c09
Sergey Danchenko <s.danchienko@mail.ru>
2021-03-24 19:34:54 +0300

    npm -v 6.14.6, node -v v12.18.4, tsc -v 4.1.5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'intrnship-app';
+  posts: Post[] = [
+    { title: 'Хочу выучить ангуляр компоненты', text: 'Все еще учу компонеты', id: 1 },
+    { title: 'Следующий блок', text: 'будет про директивы и еще про пайпы', id: 2 },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostFormComponent } from './post-form/post-form.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostFormComponent,
+    PostComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
