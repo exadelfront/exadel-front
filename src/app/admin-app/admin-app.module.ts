e346f715c4288264e461cd290f853d4a5bcae880
MarShMaloW <vladz15@ukr.net>
2021-03-31 21:46:28 +0300

    connects admin app component

--- src/app/admin-app/admin-app.module.ts
+++ src/app/admin-app/admin-app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminAppRoutingModule } from './admin-app-routing.module';
+import { AdminAppComponent } from './admin-app.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AdminAppComponent],
   imports: [
     CommonModule,
     AdminAppRoutingModule
-  ]
+  ],
+  exports:[AdminAppComponent]
 })
 export class AdminAppModule { }
