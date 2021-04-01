24b7f3c01fdbc0d7167f8650ff3608dedc2d3b99
Sergey Danchenko <s.danchienko@mail.ru>
2021-04-01 19:57:01 +0300

    feat: add rate btn component

--- src/app/admin-app/admin-app.module.ts
+++ src/app/admin-app/admin-app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminAppRoutingModule } from './admin-app-routing.module';
+import {RateBtnComponent} from './rate-btn/rate-btn.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RateBtnComponent,
+  ],
   imports: [
     CommonModule,
-    AdminAppRoutingModule
+    AdminAppRoutingModule,
   ]
 })
 export class AdminAppModule { }
