a37bfb8a9dd99fd4c146b297deba36bff4fb8253
Sergey Danchenko <s.danchienko@mail.ru>
2021-04-02 18:20:44 +0300

    feat: add posts service

---
+++ src/app/services/posts.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+export interface Post {
+  title: string;
+  tags: string[];
+  location: string;
+  description: string;
+  imageUrl: string;
+  date: string;
+  id?: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+
+  constructor(private http: HttpClient) {}
+
+  fetchEvents(): Observable<Post[]> {
+    return this.http.get<Post[]>('');
+  }
+}
