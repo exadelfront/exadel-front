4758fa61c1403b146a05b1d2c3011b8c4a48b59c
MarShMaloW <vladz15@ukr.net>
2021-04-10 12:54:55 +0300

    add info section header component

--- src/app/@shared/@shared.module.ts
+++ src/app/@shared/@shared.module.ts
@@ -20,6 +20,7 @@ import { ApproveRejectButtonComponent } from './approve-reject-button/approve-re
 import { TextareaComponent } from './textarea/textarea.component';
 import { ChooseDateComponent } from './choose-date/choose-date.component';
 import { SectionHeaderComponent } from './section-header/section-header.component';
+import { InfoSectionHeaderComponent } from './info-section-header/info-section-header.component';
 
 export function HttpLoaderFactory(http: HttpClient) {
     return new TranslateHttpLoader(http);
@@ -42,7 +43,8 @@ export function HttpLoaderFactory(http: HttpClient) {
     ApproveRejectButtonComponent,
     TextareaComponent,
     ChooseDateComponent,
-    SectionHeaderComponent],
+    SectionHeaderComponent,
+    InfoSectionHeaderComponent],
   imports: [
     CommonModule,
     HttpClientModule,
@@ -71,7 +73,8 @@ export function HttpLoaderFactory(http: HttpClient) {
     ApproveRejectButtonComponent,
     TextareaComponent,
     ChooseDateComponent,
-    SectionHeaderComponent
+    SectionHeaderComponent,
+    InfoSectionHeaderComponent
   ]
 })
 export class SharedModule { }
