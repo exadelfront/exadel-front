79da74249a1b9c5dbd7334e484d8e5c18c3faf55
MarShMaloW <vladz15@ukr.net>
2021-04-16 21:12:40 +0300

    Add routing for admin (mock-up)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {MainPageComponent} from './@core/main-page/main-page.component';
 import {PostPageComponent} from './@core/post-page/post-page.component';
-import { StudentsTablePageComponent } from './@core/students-table-page/students-table-page.component';
-import {AdminAppComponent} from './admin-app/admin-app.component';
 
 const routes: Routes = [
   {
@@ -13,14 +11,6 @@ const routes: Routes = [
   {
     path: 'post/:id',
     component: PostPageComponent,
-  },
-  {
-    path: 'admin',
-    component: AdminAppComponent
-  },
-  {
-    path: 'admin/table',
-    component: StudentsTablePageComponent
   }
 ];
 
