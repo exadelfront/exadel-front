b4470465437799be408123168fa6aba7cb4c087c
molot <you@example.com>
2021-04-16 20:17:52 +0300

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import {MainPageComponent} from './@core/main-page/main-page.component';
 import {PostPageComponent} from './@core/post-page/post-page.component';
 import { StudentsTablePageComponent } from './@core/students-table-page/students-table-page.component';
 import {AdminAppComponent} from './admin-app/admin-app.component';
+import {StudentInfoComponent} from "./@shared/student-info/student-info.component";
+import {TechnicalEstimateComponent} from "./@core/technical-estimate/technical-estimate.component";
+import {HrFormComponent} from "./admin-app/hr-form/hr-form.component";
 
 const routes: Routes = [
   {
@@ -21,6 +24,14 @@ const routes: Routes = [
   {
     path: 'admin/table',
     component: StudentsTablePageComponent
+  },
+  {
+    path: 'admin/hrform',
+    component: HrFormComponent
+  },
+  {
+    path: 'admin/techform',
+    component: TechnicalEstimateComponent
   }
 ];
 
