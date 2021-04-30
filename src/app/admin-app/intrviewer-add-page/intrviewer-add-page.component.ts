c9f9d1cfe98ff32fbf85a4d5390edb04762953e7
Sergey Danchenko <s.danchienko@mail.ru>
2021-04-30 11:29:30 +0300

    feat: add time chooser

--- src/app/admin-app/intrviewer-add-page/intrviewer-add-page.component.ts
+++ src/app/admin-app/intrviewer-add-page/intrviewer-add-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-intrviewer-add-page',
@@ -7,9 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntrviewerAddPageComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  selectDateForm: FormGroup;
+  selectedDates: string[] = ['2021-04-29T19:51', '2021-04-29T19:51', '2021-04-29T19:51'];
+
+  constructor() {
+    this.form = new FormGroup({
+      name: new FormControl(null),
+      surname: new FormControl(null),
+      email: new FormControl(null),
+      phone: new FormControl(null),
+      skype: new FormControl(null),
+    });
+    this.selectDateForm = new FormGroup({
+      selectedDate: new FormControl(null, Validators.required),
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  onAddDateBtnClick(): void {
+    if (!this.selectDateForm.invalid) {
+      const { selectedDate } = this.selectDateForm.value;
+      this.selectedDates.push(selectedDate);
+      return;
+    }
+    console.log('invalid');
+  }
+
+  deleteSelectedDate(event: MouseEvent): void {
+    const deletedDate: number = +((event.target as HTMLElement).parentNode as HTMLElement)
+      .getAttribute('index');
+    this.selectedDates = this.selectedDates.slice();
+    this.selectedDates.splice(deletedDate, 1);
+  }
+
+  dateToNormalView(strDate: string): string {
+    const [date, time] = strDate.split('T');
+    return `Date: ${date}, Time: ${time}`;
+  }
+
+  submit(): void {
+    console.log(this.form.value);
+  }
 }
