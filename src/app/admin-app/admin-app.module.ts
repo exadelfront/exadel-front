bb882f9b0c1b3f608c01b1d2018d50ffca67ffed
MarShMaloW <vladz15@ukr.net>
2021-05-13 12:18:53 +0300

    Add translate for 'add interviewer' page

--- src/app/admin-app/admin-app.module.ts
+++ src/app/admin-app/admin-app.module.ts
@@ -15,8 +15,13 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { HttpClient} from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader} from '@ngx-translate/http-loader';
 
-
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+    return new TranslateHttpLoader(http);
+}
 
 const MaterialModules = [
   MatInputModule,
@@ -41,6 +46,14 @@ const MaterialModules = [
     AdminAppRoutingModule,
     ReactiveFormsModule,
     ...MaterialModules,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      },
+      defaultLanguage: 'en'
+    }),
   ],
     exports: [
         AdminAppComponent,
